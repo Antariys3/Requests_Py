19a3461757fa7879eb6e888f0bcda49a4d92f4fc
antar <333antar333@gmail.com>
2023-09-17 15:13:38 +0200

    Добавление файла.

--- ужонд.py
+++ ужонд.py
@@ -1,39 +1,36 @@
-from requests import Session
+import requests
 # from bs4 import BeautifulSoup
-from time import sleep
-import certifi
+# from time import sleep
 
-
-url = "https://pio-przybysz.duw.pl/login"
-headers = {
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "ru,en;q=0.9",
-    "Connection": "keep-alive",
-    "Content-Length": "58",
-    "Content-Type": "application/json",
-    "Host": "api-przybysz.duw.pl",
-    "Origin": "https://pio-przybysz.duw.pl",
-    "Referer": "https://pio-przybysz.duw.pl/",
-    "sec-ch-ua": "'Chromium';v='106', 'Yandex';v='22', 'Not;A=Brand';v='99'",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "Windows",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-site",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
-                  " Chrome/106.0.0.0 YaBrowser/22.11.0.2419 Yowser/2.5 Safari/537.36"
+cookies = {
+    '_gid': 'GA1.2.1129379420.1694789566',
+    '_ga': 'GA1.1.388693999.1694789566',
+    '_ga_ZRZLNC2FN6': 'GS1.1.1694789565.1.1.1694789593.0.0.0',
 }
-url_login = "https://api-przybysz.duw.pl/api/v1/token/obtain"
 
-work = Session()
-work.get(url, headers=headers, verify=certifi.where())
-sleep(2)
-response = work.get(url, headers=headers, verify=certifi.where())
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    # 'Cookie': '_gid=GA1.2.1129379420.1694789566; _ga=GA1.1.388693999.1694789566; _ga_ZRZLNC2FN6=GS1.1.1694789565.1.1.1694789593.0.0.0',
+    'If-Modified-Since': 'Tue, 14 Jun 2022 10:00:28 GMT',
+    'If-None-Match': '"62a85c3c-529"',
+    'Referer': 'https://przybysz.duw.pl/',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+}
+
+response = requests.get('https://pio-przybysz.duw.pl/login', cookies=cookies, headers=headers)
 print(response.status_code)
 # sleep(1)
 # data = {"login": "333antar333@gmail.com", "password": "An^Tariys33"}
 # result = work.post(url_login, headers=headers, data=data, allow_redirects=True)
 # print(result.text)
-
-
