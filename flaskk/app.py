e1d3c8415fe28334b79fabd2c34f755c2884f3ed
antar <333antar333@gmail.com>
2023-09-23 18:54:53 +0200

    Додано описание.

--- flaskk/app.py
+++ flaskk/app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        # когда мы вызываем этот объект, то мы еще и вызываем его id
+        return '<Article %r>' % self.id
 
 
 @app.route("/")
@@ -14,9 +31,65 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/user/<string:name>/<int:id>")
-def user(name, id):
-    return "user page: " + name + " - " + str(id)
+@app.route("/posts")
+def posts():
+    # articles = Article.query.first()  # Будет взята первая запись из базы данных
+    articles = Article.query.order_by(Article.date.desc()).all()  # Сортировка по дате. desc, обратная сортировка
+    return render_template("posts.html", articles=articles)
+
+
+@app.route("/posts/<int:id>")
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template("post_detail.html", article=article)
+
+
+@app.route("/posts/<int:id>/del")
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect("/posts")
+    except:
+        return "При удалении статьи произошла ошибка"
+
+
+@app.route("/posts/<int:id>/update", methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect("/posts")
+        except Exception as ex:
+            return "При редактировании статьи произошла ошибка", ex
+    else:
+        return render_template("post_update.html", article=article)
+
+
+
+@app.route("/create-article", methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect("/posts")
+        except Exception as ex:
+            return "При добавлении статьи произошла ошибка", ex
+    else:
+        return render_template("create-article.html")
 
 
 if __name__ == "__main__":
